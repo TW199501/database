3bd8207feaca19831620ae6e02ab1f81a6abee5d
楊清雲 <41539966+TW199501@users.noreply.github.com>
2025-04-10 12:58:00 +0800

    1.0.12

--- metadata/reset_schemafull.sql
+++ metadata/reset_schemafull.sql
@@ -1,12 +1,12 @@
 /*
 ==============================================
-📄 檔案名稱: reset_schemafull.sql
-📁 所屬模組: meta schema
-📌 用途: 重置 schema metadata 架構（清除觸發器 + 預存程序 + 資料）
-📌 預存程序：同步 acc0_schema_tables（來自 MS_Description）         
-📅 建立日期: 2025-04-09
-👤 建立者: 楊清雲（@eddie）
-📌 適用環境: SQL Server 2022 - ELF 資料庫
+檔案名稱: reset_schemafull.sql
+所屬模組: meta schema
+用途: 重置 schema metadata 架構（清除觸發器 + 預存程序 + 資料）
+預存程序：同步 acc0_schema_tables（來自 MS_Description）         
+建立日期: 2025-04-09
+建立者: 楊清雲（@eddie）
+適用環境: SQL Server 2022 - ELF 資料庫
 ==============================================
 */
 -- 移除建表觸發器
